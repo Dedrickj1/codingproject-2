29e4f4c19f8ebdc7d42c802da5faaaaa13c31ddd
Dedrickj1 <dedrickjackson11@gmail.com>
2025-05-28 20:24:41 -0500

    update

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -3,8 +3,77 @@ import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import * as sessionActions from '../../store/session';
-import '/src/index.css'
+import '/src/index.css';
 import './Navigation.css';
+
+function Navigation({ isLoaded }) {
+  const sessionUser = useSelector(state => state.session.user);
+  const dispatch = useDispatch();
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  const logout = (e) => {
+    e.preventDefault();
+    dispatch(sessionActions.logout());
+  };
+
+  const toggleDropdown = () => {
+    setShowDropdown(prev => !prev);
+  };
+
+  const sessionLinks = sessionUser ? (
+    <>
+      <li>
+        <ProfileButton user={sessionUser} />
+      </li>
+      <li>
+        <div className="dropdown">
+          <button onClick={toggleDropdown} className="dropdown-toggle">
+            Menu ▼
+          </button>
+          {showDropdown && (
+            <ul className="dropdown-menu">
+              <li>
+                <NavLink to="/spots/current" className="dropdown-button">
+                  Manage Spots
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/reviews/current" className="dropdown-button">
+                  Manage Reviews
+                </NavLink>
+              </li>
+              <li>
+                <button onClick={logout} className="dropdown-button">
+                  Log Out
+                </button>
+              </li>
+            </ul>
+          )}
+        </div>
+      </li>
+    </>
+  ) : (
+    <>
+      <li>
+        <NavLink to="/login">Log In</NavLink>
+      </li>
+      <li>
+        <NavLink to="/signup">Sign Up</NavLink>
+      </li>
+    </>
+  );
+
+  return (
+    <ul className="nav-list">
+      {isLoaded && sessionLinks}
+    </ul>
+  );
+}
+
+export default Navigation;
+
+
+
 
 // function Navigation({ isLoaded }) {
 //   const sessionUser = useSelector(state => state.session.user);
@@ -46,63 +115,4 @@ import './Navigation.css';
 // }
 
 // export default Navigation;
-
-
-function Navigation({ isLoaded }) {
-  const sessionUser = useSelector(state => state.session.user);
-  const dispatch = useDispatch();
-  const [showDropdown, setShowDropdown] = useState(false);
-
-  const logout = (e) => {
-    e.preventDefault();
-    dispatch(sessionActions.logout());
-  };
-
-  const toggleDropdown = () => {
-    setShowDropdown(prev => !prev);
-  };
-
-  const sessionLinks = sessionUser ? (
-    <>
-      <li>
-        <ProfileButton user={sessionUser} />
-      </li>
-      <li>
-        <div className="dropdown">
-          <button onClick={toggleDropdown} className="dropdown-toggle">
-            Menu ▼
-          </button>
-          {showDropdown && (
-            <ul className="dropdown-menu">
-              <li><NavLink to="/spots/current">Manage Spots</NavLink></li>
-              <li><NavLink to="/reviews/current">Manage Reviews</NavLink></li>
-              <li><button onClick={logout}>Log Out</button></li>
-            </ul>
-          )}
-        </div>
-      </li>
-    </>
-  ) : (
-    <>
-      <li>
-        <NavLink to="/login">Log In</NavLink>
-      </li>
-      <li>
-        <NavLink to="/signup">Sign Up</NavLink>
-      </li>
-    </>
-  );
-
-  return (
-    <ul className="nav-list">
-      <li>
-        <NavLink to="/">Home</NavLink>
-      </li>
-      {isLoaded && sessionLinks}
-    </ul>
-  );
-}
-
-export default Navigation;
-
 
--- frontend/src/components/Navigation/ProfileButton.jsx
+++ frontend/src/components/Navigation/ProfileButton.jsx
@@ -1,92 +1,19 @@
-// frontend/src/components/Navigation/ProfileButton.jsx
-
-import { useState, useEffect, useRef } from 'react';
-import { useDispatch } from 'react-redux';
 import { FaHouseChimneyUser } from "react-icons/fa6";
-import * as sessionActions from '../../store/session';
-import OpenModalButton from '../OpenModalButton';
-import LoginFormModal from '../LoginFormModal';
-import SignupFormModal from '../SignupFormPage';
-import '/src/index.css';
-import './ProfileButton.css';
 import { useNavigate } from 'react-router-dom';
+import './ProfileButton.css';
 
-function ProfileButton({ user }) {
-  const dispatch = useDispatch();
-  const [showMenu, setShowMenu] = useState(false);
-  const ulRef = useRef();
+function ProfileButton() {
   const navigate = useNavigate();
 
-  const toggleMenu = (e) => {
-    e.stopPropagation();
-    setShowMenu(prev => !prev);
-  };
-
-  useEffect(() => {
-    if (!showMenu) return;
-
-    const closeMenu = (e) => {
-      if (ulRef.current && !ulRef.current.contains(e.target)) {
-        setShowMenu(false);
-      }
-    };
-
-    document.addEventListener('click', closeMenu);
-    return () => document.removeEventListener("click", closeMenu);
-  }, [showMenu]);
-
-  const logout = (e) => {
-    e.preventDefault();
-    dispatch(sessionActions.logout());
+  const goHome = () => {
     navigate('/');
-    setShowMenu(false);
-  };
-
-  const handleManageSpots = () => {
-    navigate("/spots/current");
-    setShowMenu(false);
   };
 
   return (
     <div className="profile-button-container">
-      <button onClick={toggleMenu} className="menu-button">
+      <button onClick={goHome} className="menu-button" title="Home">
         <FaHouseChimneyUser />
       </button>
-      {showMenu && (
-        <ul className="profile-dropdown" ref={ulRef}>
-          {user ? (
-            <>
-              <li>Hello, {user.firstName}</li>
-              <li>{user.email}</li>
-              <li>
-                <button onClick={handleManageSpots} className="manage-link-button prof-butt">
-                  Manage Spots
-                </button>
-              </li>
-              <li>
-                <button onClick={logout} className="prof-butt">
-                  Log Out
-                </button>
-              </li>
-            </>
-          ) : (
-            <>
-              <li>
-                <OpenModalButton
-                  buttonText="Log In"
-                  modalComponent={<LoginFormModal />}
-                />
-              </li>
-              <li>
-                <OpenModalButton
-                  buttonText="Sign Up"
-                  modalComponent={<SignupFormModal />}
-                />
-              </li>
-            </>
-          )}
-        </ul>
-      )}
     </div>
   );
 }
